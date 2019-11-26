8ddf9269c4b44b003a6dc320ae9d02a795c81164
flytreeleft <flytreeleft@crazydan.org>
2019-11-26 21:37:00 +0800

    Add Example

---
+++ example/lib/main.dart
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2019 flytreeleft<flytreeleft@crazydan.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:flutter_i18n/flutter_i18n.dart';
+
+void main() => runApp(I18nApp());
+
+class I18nApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateTitle: (context) => 'Flutter I18n Example',
+      supportedLocales: [
+        const Locale('en'),
+        const Locale('zh'),
+      ],
+      localizationsDelegates: [
+        I18n.delegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      home: HomePage(title: 'Flutter I18n Example'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _changeLanguage() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This is a text',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeLanguage,
+        tooltip: 'Change Language',
+        child: Icon(Icons.flag),
+      ),
+    );
+  }
+}
